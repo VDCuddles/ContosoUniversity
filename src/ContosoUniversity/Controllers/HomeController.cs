9399b9cabc266468bcc849b92e77324981f619f4
· Aquila · <apocalaeon@hotmail.com>
2017-03-22 19:31:33 +1300

    First Commit

---
+++ src/ContosoUniversity/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ContosoUniversity.Data;
+using ContosoUniversity.Models.SchoolViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContosoUniversity.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly SchoolContext _context;
+		public HomeController(SchoolContext context)
+		{
+			_context = context;
+		}
+
+		public IActionResult Index()
+        {
+            return View();
+        }
+
+		public async Task<ActionResult> About()
+		{
+			IQueryable<EnrollmentDateGroup> data =
+			from student in _context.Students
+			group student by student.EnrollmentDate into dateGroup
+			select new EnrollmentDateGroup()
+			{
+				EnrollmentDate = dateGroup.Key,
+				StudentCount = dateGroup.Count()
+			};
+			return View(await data.AsNoTracking().ToListAsync());
+		}
+
+
+		public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
