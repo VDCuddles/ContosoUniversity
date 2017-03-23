72df6107a065f791730a95e7204dd43f2ebf379e
· Aquila · <apocalaeon@hotmail.com>
2017-03-23 13:29:05 +1300

    saving before doing more command line stuff for databases

--- src/ContosoUniversity/Models/Student.cs
+++ src/ContosoUniversity/Models/Student.cs
@@ -1,14 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace ContosoUniversity.Models
 {
 	public class Student
 	{
 		public int ID { get; set; }
 		[StringLength(50)]
+		//[RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
 		public string LastName { get; set; }
 		[StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters.")]
+		[Column("FirstName")]
+		//[RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
 		public string FirstMidName { get; set; }
 		[DataType(DataType.Date)]
 		[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
