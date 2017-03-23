4a2b8c4c61e7cbb082e3335e84aecfbdda295916
· Aquila · <apocalaeon@hotmail.com>
2017-03-23 13:33:35 +1300

    command line stuff worked

---
+++ src/ContosoUniversity/Migrations/20170323003028_ColummFirstName.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ContosoUniversity.Migrations
+{
+    public partial class ColummFirstName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Student",
+                maxLength: 50,
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstMidName",
+                table: "Student",
+                maxLength: 50,
+                nullable: true);
+
+            migrationBuilder.RenameColumn(
+                name: "FirstMidName",
+                table: "Student",
+                newName: "FirstName");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "Student",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "Student",
+                nullable: true);
+
+            migrationBuilder.RenameColumn(
+                name: "FirstName",
+                table: "Student",
+                newName: "FirstMidName");
+        }
+    }
+}
